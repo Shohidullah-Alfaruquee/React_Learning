41ecffbf68fd494ac128e84d84ca82fdd4e31cf8
unknown <shohidullah.ru@outlook.com>
2024-01-03 05:33:03 +0600

    Static Component

---
+++ src/components/Learn.jsx
@@ -0,0 +1,13 @@
+const Learn = () => {
+    return (
+    <div>
+        <h1>Learning Topic</h1>
+        <ol>
+            <li>JavaScript</li>
+            <li>React JS</li>
+        </ol>
+    </div>
+    )
+}
+
+export default Learn;
